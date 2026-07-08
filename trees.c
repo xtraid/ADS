7b4ad512a4d530b60556f912a96f102e98dccd19
Manuel Magnabosco <magn.manuel05@gmail.com>
2026-07-08 23:50:42 +0200

    feat: added implementation of remoove of a node and find successor

--- trees.c
+++ trees.c
@@ -24,6 +24,7 @@ node * mkbst(int val){
   root->value = val;
   root->left = NULL;
   root->right = NULL;
+  root->parent = NULL;
   return root;
 }
 
@@ -43,6 +44,7 @@ int bst_insert(node *parent, int value){
     leaf->value = value;
     leaf->left = NULL;
     leaf->right = NULL;
+    leaf->parent = parent;
     parent->left = leaf;
     return 0;
   }
@@ -55,6 +57,7 @@ int bst_insert(node *parent, int value){
     leaf->value = value;
     leaf->left = NULL;
     leaf->right = NULL;
+    leaf->parent = parent;
     parent->right = leaf;
     return 0;
   }
@@ -63,5 +66,109 @@ int bst_insert(node *parent, int value){
   }
   else{
     return bst_insert(parent->right, value);
+  }
+}
+
+int rm_leaf(node * leaf){
+  if (leaf == NULL)
+    return 0;
+  if (leaf->parent == NULL){
+    free(leaf);
+    return 0;
+  }
+  if (leaf->left == NULL && leaf->right == NULL){
+    if(leaf->parent->value > leaf->value){
+      leaf->parent->left = NULL; 
+      free(leaf);
+      return 0;
+    }
+    else{
+      leaf->parent->right = NULL;
+      free(leaf);
+      return 0;
+    }
+  }
+  if (leaf->left == NULL){
+    if(leaf->parent->value > leaf->value){
+      leaf->parent->left = leaf->right;
+      leaf->right->parent = leaf->parent;
+      free(leaf);
+      return 0;
+    }
+    else{
+      leaf->parent->right = leaf->right;
+      leaf->right->parent = leaf->parent;
+      free(leaf);
+      return 0;
+    }
+  }
+  if (leaf->right == NULL){
+    if(leaf->parent->value > leaf->value){
+      leaf->parent->left = leaf->left; 
+      leaf->left->parent = leaf->parent; 
+      free(leaf);
+      return 0;
+    }
+    else{
+      leaf->parent->right = leaf->left; 
+      leaf->left->parent = leaf->parent; 
+      free(leaf);
+      return 0;
+    }
+  }
+  node *succ = find_successor(leaf); 
+  if(succ == NULL)
+    return -1;
+  succ->left = leaf->left;
+  if (succ !=leaf->right){
+    succ->parent->left = succ->right;
+    if (succ->right != NULL)
+      succ->right->parent = succ->parent;
+    succ->right = leaf->right;
+    succ->right->parent = succ;
+  }
+  succ->parent = leaf->parent;
+  leaf->left->parent = succ;
+  if (leaf->parent->value > leaf->value){
+    leaf->parent->left = succ;
+    
+    free(leaf);
+    return 0;
+  }
+  else{
+    leaf->parent->right = succ;
+    free(leaf);
+    return 0;
+  }
+
+}
+
+node * find_successor(node * leaf){
+  if (leaf == NULL)
+    return NULL;
+  node *succ;
+  if (leaf->right != NULL){
+    succ = leaf->right;
+    while(True){
+      if (succ->left != NULL)
+        succ = succ->left;
+      else
+        break;
+    }
+  return succ;
+  }
+  else{
+    succ = leaf->parent;
+    int buf = leaf->value;
+    while (True){
+      if (succ == NULL)
+        return NULL;
+      if (succ->value > buf)
+        break;
+      else
+        buf = succ->value;
+        succ = succ->parent;
+    }
+    return succ;
   }
 }
--- trees.h
+++ trees.h
@@ -1,14 +1,17 @@
 #ifndef trees
 #define trees
 
+enum{False, True};
 
 typedef struct node{
   int value;
   struct node *left;
   struct node *right;
+  struct node *parent;
 }node;
 
 
 node * mkbst(int val);
 int bst_insert(node *parent, int value);
+int rm_leaf(node *leaf);
 #endif
