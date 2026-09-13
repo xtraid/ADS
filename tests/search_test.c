654ee70e23b32363b6af7a6e2824001c9f770a25
Manuel Magnabosco <magn.manuel05@gmail.com>
2026-09-13 07:00:00 +0200

    test: update search tests

--- tests/search_test.c
+++ tests/search_test.c
@@ -6,8 +6,8 @@
 // helpers
 static void unsorted_initialization (int *arr, int n);
 static void sorted_initialization (int *arr, int n);
-static void linear_search_test(int *unsorted, int *sorted, int n);
-static void binary_search_test(int *sorted, int n);
+static int linear_search_test(int *unsorted, int *sorted, int n);
+static int binary_search_test(int *sorted, int n);
 
 void unsorted_initialization (int *arr, int n){
   if(!arr)
@@ -23,41 +23,55 @@ static void sorted_initialization (int *arr, int n){
     arr[i] = 2 * i;
 }
 
-static void linear_search_test( int *unsorted, int *sorted, int n){
+int linear_search_test( int *unsorted, int *sorted, int n){
+  int fail = 0;
 
   int expected_check = linear_search(unsorted, n, 5);
-  if(expected_check != 5)
-    printf("- Error: function returned an index different fron 4 : arr[%d] = %d\n", expected_check, unsorted[expected_check]);
+  if(expected_check != 5){
+    printf("- Error: function returned an index different fron 4: %d\n", expected_check);
+    fail++;
+  }
   else
     printf("- in bound key: pass\n");
 
   int out_of_bound_key = linear_search(unsorted, n, n+1);
-  if(out_of_bound_key != -1)
+  if(out_of_bound_key != -1){
     printf("- Error: called an out of bound index got:  %d\n", out_of_bound_key);
+    fail++;
+  }
   else
     printf("- out of bound key: pass\n");
   int first_element = sorted[0];
   int solution = linear_search(sorted, n, first_element);
-  if (solution != 0)
-    printf("- Error: binary search for %d returned index: %d that references sorted[%d] = %d\n", first_element, solution, solution, sorted[solution]);
+  if (solution != 0){
+    printf("- Error: binary search for %d returned index: %d\n", first_element, solution);
+    fail++;
+  }
   else
     printf("- first element found: pass\n");
+
   int err = linear_search(NULL, n, n);
-  if (err != -2)
+  if (err != -2){
     printf("- Error: passed a NULL pointer and got  %d\n", err);
+    fail++;
+  }
   else
     printf("- broken pointer check: pass\n");
 
-
+  return fail;
 
 }
 
 
-static void binary_search_test(int *sorted, int n){
+int binary_search_test(int *sorted, int n){
+  int fail = 0;
+
   int first_element = sorted[0];
-  int solution = b_search (sorted, 0, n, first_element);
-  if (solution != 0)
-    printf("- Error: binary search for %d returned index: %d that references sorted[%d] = %d\n", first_element, solution, solution, sorted[solution]);
+  int solution = b_search (sorted, 0, n-1, first_element);
+  if (solution != 0){
+    printf("- Error: binary search for %d returned index: %d\n", first_element, solution);
+    fail++;
+  }
   else
     printf("- first element found: pass\n");
 
@@ -66,37 +80,49 @@ static void binary_search_test(int *sorted, int n){
   if (solution == -1){
     printf("- absent value: pass\n");
   }
-  else
+  else{
     printf("- Error: b_search returned %d\n", solution);
+    fail++;
+  }
 
   int test = sorted[m];  // ill look into a random element in the array if he finds it
   solution = b_search(sorted, 0, n - 1, test);
-  if (solution != m)
-    printf("- Error: binary search for %d returned index %d which is %d\n", test, solution, sorted[solution]);
+  if (solution != m){
+    printf("- Error: binary search for %d returned index %d\n", test, solution);
+    fail++;
+  }
   else
     printf("- random middle element: pass\n");
 
   test = sorted[0] - 1;
   solution = b_search(sorted, 0, n - 1, test);
-  if (solution != -1)
+  if (solution != -1){
     printf("- Error: out of domain value didnt give the right error: %d\n",solution);
+    fail++;
+  }
   else
     printf("- absent out of bound <: pass\n");
 
   test = sorted[n-1] + 1;
   solution = b_search(sorted, 0, n - 1, test);
-  if (solution != -1)
+  if (solution != -1){
     printf("- Error: out of domain value didnt give the right error: %d\n",solution);
+    fail++;
+  }
   else
     printf("- absent out of bound >: pass\n");
 
   int * mono = malloc(sizeof (int));
   mono[0]= 1;
-  solution = b_search(mono, 0, 1, 1);
-  if(solution != 0)
+  solution = b_search(mono, 0, 0, 1);
+  if(solution != 0){
     printf("- Error: has problem with array of 1 elements\n");
+    fail++;
+  }
   else
     printf("- single element array: pass\n");
+  free(mono);
+  return fail;
 }
 
 
@@ -106,6 +132,7 @@ int main (void){
   int *unsorted = malloc(sizeof(int) * n);
   int *sorted = malloc(sizeof(int) * n);
 
+  int fail = 0;
 
   if (!unsorted){
     printf("malloc failed\n");
@@ -118,11 +145,11 @@ int main (void){
   printf("correctness tests:\n");
   printf("\nlinear sort test:\n");
 
-  linear_search_test(unsorted, sorted, n);
+  fail += linear_search_test(unsorted, sorted, n);
 
   printf("\nbinary search tests:\n");
 
-  binary_search_test(sorted, n);
+  fail += binary_search_test(sorted, n);
 
 
 
